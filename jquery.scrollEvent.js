95615720ff29a88b3730f893322eda5416cae553
Pavel Kulbakin <p.kulbakin@gmail.com>
2016-01-10 13:22:08 +0300

    rename package

--- jquery.scrollEvent.js
+++ jquery.scrollEvent.js
@@ -1,22 +1,39 @@
 /**
+ * jQuery.scrollEvent.js
+ *
+ * @author Pavel Kulbakin
+ * @version 1.0.0
+ * @license MIT
+ *
  * Introduce scrollstart, scrollstop, scrolldown ans scrollup events
  */
-(function ($) {
+(function (factory) {
+    if (typeof define === "function" && define.amd) {
+        /** AMD. Register as an anonymous module. */
+        define(["jquery"], factory);
+    } else if (typeof module === "object" && module.exports) {
+        /** Node/CommonJS */
+        module.exports = factory(require("jquery"));
+    } else {
+        /** Browser globals */
+        factory(window.jQuery);
+    }
+}(function ($) {
     var scrollEvent = "touchmove scroll";
-    
+
     // setup new event shortcuts
     $.each(["scrollstart", "scrollstop", "scrolldown", "scrollup"], function (i, name) {
-        
+
         $.fn[name] = function (fn) {
             return fn ? this.bind(name, fn) : this.trigger(name);
         };
-        
+
         // jQuery < 1.8
         if ($.attrFn) {
             $.attrFn[name] = true;
         }
     });
-    
+
     function triggerCustomEvent(obj, eventType, event, bubble) {
         var originalType = event.type;
         event.type = eventType;
@@ -27,7 +44,7 @@
         }
         event.type = originalType;
     }
-    
+
     // also handles scrollstop, scrolldown, scrollup
     $.event.special.scrollstart = {
         setup: function () {
@@ -36,24 +53,24 @@
                 scrolling,
                 timer,
                 scrollTop;
-            
+
             function trigger(event, state) {
                 scrolling = state;
                 triggerCustomEvent(thisObject, scrolling ? "scrollstart" : "scrollstop", event);
             }
-            
+
             // iPhone triggers scroll after a small delay; use touchmove instead
             $this.bind(scrollEvent, function (event) {
                 if ( ! scrolling) {
                     trigger(event, true);
                 }
-                
+
                 var st = $this.scrollTop();
                 if (null !== scrollTop && 0 != st - scrollTop) {
                     triggerCustomEvent(thisObject, 0 < st - scrollTop? "scrolldown" : "scrollup", event);
                 }
                 scrollTop = st;
-                
+
                 clearTimeout(timer);
                 timer = setTimeout(function () {
                     trigger(event, false);
@@ -65,7 +82,7 @@
             $(this).unbind(scrollEvent);
         }
     };
-    
+
     $.each(["scrollstop", "scrolldown", "scrollup"], function (i, event) {
         $.event.special[event] = {
             setup: function() {
@@ -76,4 +93,4 @@
             }
         };
     });
-})(jQuery);
+}));
